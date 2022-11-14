21704d7de73caf8dd86fa128d26991a0b56dd40e
hs <hyeonsu0311@knu.ac.kr>
2022-11-14 16:59:47 +0900

    git push test

--- app/src/main/java/com/example/a24mo/MainActivity.kt
+++ app/src/main/java/com/example/a24mo/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.a24mo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//git push test
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
