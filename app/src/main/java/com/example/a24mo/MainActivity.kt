908424e05ac7bcb68b14c04f801f6869d40bfef4
LimSeungW <112800645+LimSeungW@users.noreply.github.com>
2022-11-28 21:42:34 +0900

    개인 저장용

--- app/src/main/java/com/example/a24mo/MainActivity.kt
+++ app/src/main/java/com/example/a24mo/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.example.a24mo
 
+import android.app.Dialog
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.Window
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.denzcoskun.imageslider.constants.ScaleTypes
 
-class MainActivity : AppCompatActivity(), View.OnClickListener{
-    //슬라이드할 이미지 리스트
+class MainActivity : AppCompatActivity(){
+    //화면전환 인텐트 관련 코드
+    lateinit var requestLaunch : ActivityResultLauncher<Intent>
+    val Recommend_wine_code = 100 //액티비티를 구분할 코드들
+    val DetailSearch_wine_code = 200
+    val manager_menu_code = 200
 
+    //슬라이드할 이미지 리스트
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_layout) //홈화면 레이아웃 xml
@@ -33,36 +46,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
                 //선택한 이미지 = position(ex. 첫번재사진  -> 0 / 두번째사진 -> 1 ...)
             }
         })
+        //액티비티 화면전환 관련
+        requestLaunch = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            if(it.resultCode == RESULT_OK)
+            {
+
+            }
+        }
 
         //와인검색,추천 버튼
         var Recommend_Wine : Button = findViewById(R.id.Recommend_Wine_btn) // 와인추천버튼
+        Recommend_Wine.setOnClickListener {
+            val intent = Intent(this, Recommend_Wind::class.java )
+            requestLaunch.launch(intent)
+        }
         var Search_Wine : Button = findViewById(R.id.Detail_Search_btn) // 와인상세검색 버튼
-        Recommend_Wine.setOnClickListener(this)
-        Search_Wine.setOnClickListener(this)
+        Search_Wine.setOnClickListener {
+
+        }
 
         //장바구니, 바코드 버튼
         var Shopping_Basket : Button = findViewById(R.id.Shopping_Basket) // 장바구니버튼
-        Shopping_Basket.setOnClickListener(this)
+        Shopping_Basket.setOnClickListener {
+
+        }
+
         var Bar_Cord : Button = findViewById(R.id.BarCord) //바코드 버튼
         Bar_Cord.setOnClickListener{show_Scanner()}
 
         //관리자메뉴
         var manager_menu : ImageButton = findViewById(R.id.Manager_Btn)
-        manager_menu.setOnClickListener(this)
-    }
-//    버튼 이벤트
-    override fun onClick(v: View?) {
+        manager_menu.setOnClickListener {
 
-        if(v?.id == R.id.Recommend_Wine_btn) //와인추천버튼 이벤트처리
-        {}
-        else if(v?.id == R.id.Detail_Search_btn) //와인상세검색 버튼 이벤트처리
-        {}
-        else if(v?.id == R.id.Shopping_Basket) //장바구니 버튼 이벤트 처리
-        {}
-        else if(v?.id == R.id.Manager_Btn) //관리자메뉴 버튼 이벤트 처리
-        {}
-
+        }
     }
+
     //이미지 슬라이드 이미지 추가함수.
     fun Add_Image_list (): ArrayList<SlideModel>{
         //SlideModel("url", "제목")
@@ -74,13 +94,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         image_list.add(SlideModel("https://wine21.speedgabia.com/WINE_MST/IMAGE/0170000/T0170722_001.png", ScaleTypes.FIT))
         return image_list
     }
+
     //바코드 다이얼로그 출력
     fun show_Scanner(){
         val builder : AlertDialog.Builder = AlertDialog.Builder(this)
         val inflater : LayoutInflater = layoutInflater
         builder.setView(inflater.inflate(R.layout.barcode_dialog, null))
-        builder.setNegativeButton("돌아가기"){dialog,p1 -> dialog.cancel()}
+//        builder.setNegativeButton("돌아가기"){dialog,p1 -> dialog.cancel()}
         val alertDialog : AlertDialog = builder.create()
+        alertDialog?.setCanceledOnTouchOutside(true)  //바깥 터치시 다이얼로그 사라짐
+        alertDialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        alertDialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
         alertDialog.show()
     }
 
