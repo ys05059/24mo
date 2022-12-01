158a65d71519be2956ccd8787def936b8363406c
HeewonP825 <7hwon@naver.com>
2022-12-02 08:55:44 +0900

    rm mainActivity

--- app/src/main/java/com/example/a24mo/MainActivity.kt
+++ app/src/main/java/com/example/a24mo/MainActivity.kt
@@ -6,19 +6,19 @@ import androidx.fragment.app.FragmentActivity
 import com.example.a24mo.shoppingCart.ShoppingCartDialogFragment
 
 class MainActivity : FragmentActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val button = findViewById<Button>(R.id.button);
-
-        button.setOnClickListener{
-            val fragment1 = ShoppingCartDialogFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.linearlayout, fragment1)
-                .commit()
-        }
-    }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val button = findViewById<Button>(R.id.button);
+//
+//        button.setOnClickListener{
+//            val fragment1 = ShoppingCartDialogFragment()
+//            supportFragmentManager
+//                .beginTransaction()
+//                .add(R.id.linearlayout, fragment1)
+//                .commit()
+//        }
+//    }
 }
