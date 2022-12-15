d7298876514b5c53e4344ac48d4a8ba23a5b0471
HeewonP825 <7hwon@naver.com>
2022-12-15 15:29:53 +0900

    detail search

--- app/src/main/java/Fragment/Dialog_Classes.kt
+++ app/src/main/java/Fragment/Dialog_Classes.kt
@@ -356,3 +356,392 @@ class TasteDialog(context:Context)
     }
 }
 
+//생산지
+class CountryDialog(context: Context){
+    private val dialog = Dialog(context)
+    //레이아웃을 보여줌.
+    fun Show(){
+        dialog.setContentView(R.layout.detail_country)
+        dialog.window!!.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) //다이얼로그 테두리 둥글게
+
+        var count_Item = 0 //1개만 선택가능
+        var select_index = -1
+        val CountryList  = ArrayList<Button>()
+        CountryList.add(dialog.findViewById<Button>(R.id.France))//고기류 0
+        CountryList.add(dialog.findViewById<Button>(R.id.italy))//해산물 1
+        CountryList.add(dialog.findViewById<Button>(R.id.spain)) //치즈류 2
+        CountryList.add(dialog.findViewById<Button>(R.id.usa))//과일류 3
+        CountryList.add(dialog.findViewById<Button>(R.id.chile))// 디저트류 4
+        CountryList.add(dialog.findViewById<Button>(R.id.etc))//기타 5
+        val done = dialog.findViewById<Button>(R.id.done_btn) //완료버튼
+
+        for (list in CountryList){
+            Change_font_size(list)
+        }
+        //반복문 안에넣으면 에러가남
+        CountryList[0].setOnClickListener {
+            if(count_Item == 0)
+            {
+                CountryList[0].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                CountryList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                CountryList[0].setBackgroundResource(R.drawable.button_round_white_red)
+            }
+            select_index = 0
+        }
+
+        CountryList[1].setOnClickListener {
+            if(count_Item == 0)
+            {
+                CountryList[1].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                CountryList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                CountryList[1].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 1
+        }
+
+        CountryList[2].setOnClickListener {
+            if(count_Item == 0)
+            {
+                CountryList[2].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                CountryList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                CountryList[2].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 2
+        }
+
+        CountryList[3].setOnClickListener {
+            if(count_Item == 0)
+            {
+                CountryList[3].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                CountryList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                CountryList[3].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 3
+        }
+
+        CountryList[4].setOnClickListener {
+            if(count_Item == 0)
+            {
+                CountryList[4].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                CountryList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                CountryList[4].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 4
+        }
+
+        CountryList[5].setOnClickListener {
+            if(count_Item == 0)
+            {
+                CountryList[5].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                CountryList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                CountryList[5].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 5
+        }
+
+
+        //완료버튼시 리스너
+        done.setOnClickListener {
+            onClickedListener.onClicked(select_index)
+            //뭘선택했는지는 Detail_Search_Fragment1에서 뷰모델로 넘김
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+    interface ButtonClickListener{
+        fun onClicked(select_index:Int) //인터페이스를 통해 프래그먼트에서 값을 받을수있음.
+    }
+    private lateinit var onClickedListener: ButtonClickListener
+    fun setOnClickedListener(listener: ButtonClickListener) {
+        onClickedListener = listener
+    }
+    //클릭이벤트 작동방식은 위의 가격대 다이얼로그와 동일
+
+    fun Change_font_size(btn : Button){
+        var content = btn.text.toString() + " "
+        var spanningString : SpannableString = SpannableString(content)
+        val start : Int = content.indexOf(" ") //문자 폰트를 바꿀 시작지점
+        val end : Int = content.length //끝지점
+        //0.5f ->기존보다 0.5배
+        spanningString.setSpan(RelativeSizeSpan(0.5f),start,end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+        btn.setText(spanningString)
+    }
+
+
+}
+
+//도수
+class AlcoholDialog(context: Context){
+    private val dialog = Dialog(context)
+    //레이아웃을 보여줌.
+    fun Show(){
+        dialog.setContentView(R.layout.detail_alcohol)
+        dialog.window!!.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) //다이얼로그 테두리 둥글게
+
+        var count_Item = 0 //1개만 선택가능
+        var select_index = -1
+        val AlcoholList  = ArrayList<Button>()
+        AlcoholList.add(dialog.findViewById<Button>(R.id.nonalcohol))//고기류 0
+        AlcoholList.add(dialog.findViewById<Button>(R.id.lowAlcohol))//해산물 1
+        AlcoholList.add(dialog.findViewById<Button>(R.id.mid_alcohol)) //치즈류 2
+        AlcoholList.add(dialog.findViewById<Button>(R.id.high_alcohol))//과일류 3
+
+        val done = dialog.findViewById<Button>(R.id.done_btn) //완료버튼
+
+        for (list in AlcoholList){
+            Change_font_size(list)
+        }
+        //반복문 안에넣으면 에러가남
+        AlcoholList[0].setOnClickListener {
+            if(count_Item == 0)
+            {
+                AlcoholList[0].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                AlcoholList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                AlcoholList[0].setBackgroundResource(R.drawable.button_round_white_red)
+            }
+            select_index = 0
+        }
+
+        AlcoholList[1].setOnClickListener {
+            if(count_Item == 0)
+            {
+                AlcoholList[1].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                AlcoholList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                AlcoholList[1].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 1
+        }
+
+        AlcoholList[2].setOnClickListener {
+            if(count_Item == 0)
+            {
+                AlcoholList[2].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                AlcoholList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                AlcoholList[2].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 2
+        }
+
+        AlcoholList[3].setOnClickListener {
+            if(count_Item == 0)
+            {
+                AlcoholList[3].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                AlcoholList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                AlcoholList[3].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 3
+        }
+
+        //완료버튼시 리스너
+        done.setOnClickListener {
+            onClickedListener.onClicked(select_index)
+            //뭘선택했는지는 Detail_Search_Fragment1에서 뷰모델로 넘김
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+    interface ButtonClickListener{
+        fun onClicked(select_index:Int) //인터페이스를 통해 프래그먼트에서 값을 받을수있음.
+    }
+    private lateinit var onClickedListener: ButtonClickListener
+    fun setOnClickedListener(listener: ButtonClickListener) {
+        onClickedListener = listener
+    }
+    //클릭이벤트 작동방식은 위의 가격대 다이얼로그와 동일
+
+    fun Change_font_size(btn : Button){
+        var content = btn.text.toString() + " "
+        var spanningString : SpannableString = SpannableString(content)
+        val start : Int = content.indexOf(" ") //문자 폰트를 바꿀 시작지점
+        val end : Int = content.length //끝지점
+        //0.5f ->기존보다 0.5배
+        spanningString.setSpan(RelativeSizeSpan(0.5f),start,end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+        btn.setText(spanningString)
+    }
+
+
+}
+
+//종류
+class kindDialog(context: Context){
+    private val dialog = Dialog(context)
+    //레이아웃을 보여줌.
+    fun Show(){
+        dialog.setContentView(R.layout.detail_kind)
+        dialog.window!!.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) //다이얼로그 테두리 둥글게
+
+        var count_Item = 0 //1개만 선택가능
+        var select_index = -1
+        val KindList  = ArrayList<Button>()
+        KindList.add(dialog.findViewById<Button>(R.id.red))//고기류 0
+        KindList.add(dialog.findViewById<Button>(R.id.white))//해산물 1
+        KindList.add(dialog.findViewById<Button>(R.id.rose)) //치즈류 2
+        KindList.add(dialog.findViewById<Button>(R.id.sparkling))//과일류 3
+        KindList.add(dialog.findViewById<Button>(R.id.strongAlcohol))// 디저트류 4
+
+        val done = dialog.findViewById<Button>(R.id.done_btn) //완료버튼
+
+        for (list in KindList){
+            Change_font_size(list)
+        }
+        //반복문 안에넣으면 에러가남
+        KindList[0].setOnClickListener {
+            if(count_Item == 0)
+            {
+                KindList[0].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                KindList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                KindList[0].setBackgroundResource(R.drawable.button_round_white_red)
+            }
+            select_index = 0
+        }
+
+        KindList[1].setOnClickListener {
+            if(count_Item == 0)
+            {
+                KindList[1].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                KindList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                KindList[1].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 1
+        }
+
+        KindList[2].setOnClickListener {
+            if(count_Item == 0)
+            {
+                KindList[2].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                KindList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                KindList[2].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 2
+        }
+
+        KindList[3].setOnClickListener {
+            if(count_Item == 0)
+            {
+                KindList[3].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                KindList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                KindList[3].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 3
+        }
+
+        KindList[4].setOnClickListener {
+            if(count_Item == 0)
+            {
+                KindList[4].setBackgroundResource(R.drawable.button_round_white_red)
+                count_Item++
+
+            }
+            else{
+                KindList[select_index].setBackgroundResource(R.drawable.button_round_white)
+                KindList[4].setBackgroundResource(R.drawable.button_round_white_red)
+
+            }
+            select_index = 4
+        }
+
+        //완료버튼시 리스너
+        done.setOnClickListener {
+            onClickedListener.onClicked(select_index)
+            //뭘선택했는지는 Detail_Search_Fragment1에서 뷰모델로 넘김
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+    interface ButtonClickListener{
+        fun onClicked(select_index:Int) //인터페이스를 통해 프래그먼트에서 값을 받을수있음.
+    }
+    private lateinit var onClickedListener: ButtonClickListener
+    fun setOnClickedListener(listener: ButtonClickListener) {
+        onClickedListener = listener
+    }
+    //클릭이벤트 작동방식은 위의 가격대 다이얼로그와 동일
+
+    fun Change_font_size(btn : Button){
+        var content = btn.text.toString() + " "
+        var spanningString : SpannableString = SpannableString(content)
+        val start : Int = content.indexOf(" ") //문자 폰트를 바꿀 시작지점
+        val end : Int = content.length //끝지점
+        //0.5f ->기존보다 0.5배
+        spanningString.setSpan(RelativeSizeSpan(0.5f),start,end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+        btn.setText(spanningString)
+    }
+
+
+}
